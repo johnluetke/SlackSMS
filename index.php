0e1d6501d1503179426fce102c27107327d7d856
johnluetke <john@johnluetke.net>
2017-01-06 10:41:37 -0800

    Redirect to https

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ use Katzgrau\KLogger\Logger;
 use Psr\Log\LogLevel;
 
 use SMS\Config;
-use SMS\SMS;
 use SMS\SlackClient;
 use SMS\Controllers\EventController;
 use SMS\Controllers\SlashCommandController;
@@ -23,7 +22,7 @@ use SMS\Controllers\TwilioController;
 $app = new \Silex\Application();
 $app->register(new \Silex\Provider\SessionServiceProvider());
 
-$logger = new Logger("logs", LogLevel::INFO, array(
+$logger = new Logger("logs", LogLevel::DEBUG, array(
     "logFormat" => "[{date}]\t[{level}{level-padding}]\t{message}]"
 ));
 $slack = new SlackClient('xoxp-no-token', $logger);
@@ -55,9 +54,22 @@ $authenticate = function(Request $request) use ($app, $slack, $logger) {
 		}
         else {
             $logger->notice(print_r($response, true));
-            $logger->info(sprintf("Installing for new team %s", $team));
+            $logger->info(sprintf("Installing for new team %s", $response['team_id']));
             $config = Config::createConfig($response['team_id'], $response['access_token'], $response['bot']['bot_access_token'], $response['bot']['bot_user_id']);
+            error_log(print_r($config, true));
             $config->save();
+            error_log(print_r($config, true));
+
+            die("post save");
+
+            $slack->setToken($response['bot']['bot_access_token']);
+            $slack->sendDirectMessage($response['user_id'], "Welcome! Thanks for installing SMS!");
+            if (!$slack->getUser($response['user_id'])['is_admin']) {
+                $slack->sendDirectMessage($response['user_id'], "Please contact your team admin in order to configure your SMS provider");
+            }
+            else {
+                $slack->sendDirectMessage($response['user_id'], "In order to finish setting up SMS, you'll need to configure your SMS provider. The see a list of available providers, type `/sms config providers`");
+            }
 
 			return $app->redirect(sprintf("%s://%s%s", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl()));
 		}
@@ -90,13 +102,19 @@ $authenticate = function(Request $request) use ($app, $slack, $logger) {
 	}
 };
 
+$forceHttps = function (Request $request) use ($app) {
+    if ($request->getScheme() !== "https") {
+        return $app->redirect(sprintf("https://%s%s", $request->getHttpHost(), $request->getBaseUrl()));
+    }
+};
+
 $validate = function(Request $request) use ($app, $logger) {
     if ($request->request->get("token") !== SLACK_COMMAND_TOKEN) {
         $logger->error(sprintf("Invalid command token %s", $request->request->get("token")));
         return $app->abort(400);
     }
 
-    if (!SMS::isInstalledFor($request->request->get("team_id"))) {
+    if (!Config::isInstalledFor($request->request->get("team_id"))) {
         $logger->error(sprintf("Team %s not configured to use app", $request->request->get("team_id")));
         $app->abort(400);
     }
@@ -112,11 +130,15 @@ $app->error(function(Exception $e, $code) use ($logger) {
 
 $app->get("/", function () {
     return "There's nothing here :-(";
-})->before($authenticate);
+})
+->before($forceHttps)
+->before($authenticate);
 
 $app->get('/install', function () use ($app, $slack) {
     die("/install");
-})->before($authenticate);
+})
+->before($forceHttps)
+->before($authenticate);
 
 $app->mount("/cmd", new SlashCommandController($slack, $logger));
 $app->mount("/event", new EventController($slack, $logger));
