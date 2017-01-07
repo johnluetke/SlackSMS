f00cc694281743babf2204bcfe515127b3706b3a
John <john@johnluetke.net>
2017-01-06 23:35:22 -0800

    Re-arch install path

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 use Frlnc\Slack\Core\Commander;
 use Frlnc\Slack\Http\CurlInteractor;
@@ -30,65 +31,13 @@ $slack = new SlackClient('xoxp-no-token', $logger);
 
 
 function oauth_request(Application $app, Request $request, Array $scopes) {
-	$query = http_build_query(array(
-		"client_id" => SLACK_CLIENT_ID,
-		"scope" => implode(",", $scopes),
-		"redirect_uri" => sprintf("%s://%s%s", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl())
-	));
-	return $app->redirect(SLACK_OAUTH_URL . "?" . $query);
+    $query = http_build_query(array(
+        "client_id" => SLACK_CLIENT_ID,
+        "scope" => implode(",", $scopes),
+        "redirect_uri" => sprintf("%s://%s%s/install", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl())
+    ));
+    return $app->redirect(SLACK_OAUTH_URL . "?" . $query);
 }
-
-$authenticate = function(Request $request) use ($app, $slack, $logger) {
-	$token = $app['session']->get("token");
-
-	if (isset($_REQUEST['code'])) {
-		$code = $_REQUEST['code'];
-		$response = $slack->execute("oauth.access", [
-	        	'client_id' => SLACK_CLIENT_ID,
-	        	'client_secret' => SLACK_CLIENT_SECRET,
-	        	'code' => $code,
-        		'redirect_uri' => sprintf("%s://%s%s", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl())
-		])->getBody();
-
-		if (!$response['ok']) {
-			throw new Exception($response['error']);
-		}
-        else {
-            $logger->notice(print_r($response, true));
-            $logger->info(sprintf("Installing for new team %s", $team));
-            $config = Config::createConfig($response['team_id'], $response['access_token'], $response['bot']['bot_access_token'], $response['bot']['bot_user_id']);
-            $config->save();
-
-			return $app->redirect(sprintf("%s://%s%s", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl()));
-		}
-	}
-
-	if ($token === null) {
-		return oauth_request($app, $request, ['bot', 'commands', 'channels:write', 'groups:write', 'channels:read', 'groups:read']);
-	}
-	else {
-		$slack->setToken($token);
-
-		$required_scopes = ['identify', 'bot', 'commands', 'channels:write'];
-		$needed_scopes = array_diff($required_scopes, $app['session']->get("scopes"));
-
-		if (sizeof($needed_scopes) > 0) {
-			return oauth_request($app, $request, $needed_scopes);
-		}
-
-		if ($app['session']->get("name") === null) {
-			$response = $slack->execute("users.identity")->getBody();
-	
-			if (!$response['ok']) {
-				throw new Exception($response['error']);
-			}
-			else {
-				$app['session']->set("uid", $response['user']['id']);
-				$app['session']->set("name", $response['user']['name']);
-			}
-		}
-	}
-};
 
 $validate = function(Request $request) use ($app, $logger) {
     if ($request->request->get("token") !== SLACK_COMMAND_TOKEN) {
@@ -106,17 +55,95 @@ $app->error(function(Exception $e, $code) use ($logger) {
     //die($request->getBaseUrl());
     $logger->error($e->getMessage());
     $logger->error($e->getTraceAsString());
-	// Eventually, have a custom error page
-	return $e->getMessage();
+    // Eventually, have a custom error page
+    return $e->getMessage();
 });
 
 $app->get("/", function () {
     return "There's nothing here :-(";
-})->before($authenticate);
+});
 
-$app->get('/install', function () use ($app, $slack) {
-    die("/install");
-})->before($authenticate);
+$app->get('/install', function(Request $request) use ($app, $logger, $slack) {
+    if ($request->query->has("code")) {
+        $code = $request->query->get("code");
+        $response = $slack->execute("oauth.access", [
+                'client_id' => SLACK_CLIENT_ID,
+                'client_secret' => SLACK_CLIENT_SECRET,
+                'code' => $code,
+                'redirect_uri' => sprintf("%s://%s%s/install", $request->getScheme(), $request->getHttpHost(), $request->getBaseUrl())
+        ])->getBody();
+
+        if (!$response['ok']) {
+            throw new Exception($response['error']);
+        }
+
+        if (Config::isInstalledFor($response['team_id'])) {
+            return $app->redirect(sprintf("/already-installed?team=%s", $response['team_id']));
+        }
+        else {
+            $logger->info(sprintf("Installing for new team %s", $response['team_id']));
+            $app['session']->set("token", $response['access_token']);
+            $app['session']->set("team", $response['team_id']);
+
+            $installRequest = Request::create("/install", "POST", array(
+                "team_id" => $response['team_id'],
+                "access_token" => $response['access_token'],
+                "bot_access_token" => $response['bot']['bot_access_token'],
+                "bot_user_id" => $response['bot']['bot_user_id']
+            ));
+            $app->handle($installRequest, HttpKernelInterface::SUB_REQUEST, false);
+            return $app->redirect("/install");
+        }
+    }
+    else {
+        $token = $app['session']->get("token");
+    
+        if ($token === null) {
+            $logger->debug("No token. Redirecting to Slack.");
+            return oauth_request($app, $request, ['bot', 'commands', 'channels:write', 'groups:write', 'channels:read', 'groups:read']);
+        }
+        else {
+            if (!Config::isInstalledFor($app['session']->get("team"))) {
+                $logger->warning(sprintf("Have a token, but not installed for team %s. Reinstalling", $app['session']->get("team")));
+                $app['session']->set("token", null);
+                return $app->redirect("/install");
+            }
+
+            $logger->debug("Slack token possessed.");
+            $slack->setToken($token);
+    
+            $required_scopes = ['identify', 'bot', 'commands', 'channels:write'];
+            $needed_scopes = array_diff($required_scopes, $app['session']->get("scopes"));
+    
+            if (sizeof($needed_scopes) > 0) {
+                $logger->info(sprintf("Still need scope(s): %s. Redirecting to Slack.", print_r($needed_scopes, true)));
+                return oauth_request($app, $request, $needed_scopes);
+            }
+    
+            if ($app['session']->get("name") === null) {
+                $response = $slack->execute("users.identity")->getBody();
+        
+                if (!$response['ok']) {
+                    throw new Exception($response['error']);
+                }
+                else {
+                    $app['session']->set("uid", $response['user']['id']);
+                    $app['session']->set("name", $response['user']['name']);
+                }
+            }
+    
+            return $app->redirect("/welcome");
+        }
+    }
+});
+
+$app->post('/install', function (Request $request) use ($app, $logger, $slack) {
+    $logger->notice(print_r($request, true));
+    $config = Config::createConfig($request->request->get("team_id"), $request->request->get("access_token"), $request->request->get("bot_access_token"), $request->request->get("bot_user_id"));
+    $config->save();
+
+    return new Response(200);
+});
 
 $app->mount("/cmd", new SlashCommandController($slack, $logger));
 $app->mount("/event", new EventController($slack, $logger));
